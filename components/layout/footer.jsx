0b66a648b5220314fd14be6f1f316e16c8dba21d
Siddesh Bagare <sbagare@medforce.net>
2025-05-25 02:08:49 +0530

    footer fixed

--- components/layout/footer.jsx
+++ components/layout/footer.jsx
@@ -31,43 +31,9 @@ export default function Footer() {
     <footer className={css.container}>
       <Container spacing={["verticalXXLrg", "bottomLrg"]}>
         <section className={css.sections}>
-          <ul className={css.thanks}>
-            <li>
-              <h4>Acknowledgments</h4>
-            </li>
-            {content.acknowledgments.map(({ person, link, note }, index) => {
-              return (
-                <li key={index}>
-                  <a href={link} rel="noreferrer" target="_blank">
-                    {person}{" "}
-                    <Icon icon={["fad", "arrow-up-right-from-square"]} />
-                  </a>
-                  <p>{note}</p>
-                </li>
-              );
-            })}
-          </ul>
-          <ul className={css.links}>
-            <li>
-              <h4>Links</h4>
-            </li>
-            {content.links.map(({ person, link, note }, index) => {
-              return (
-                <li key={index}>
-                  <a href={link} rel="noreferrer" target="_blank">
-                    {person}{" "}
-                    <Icon icon={["fad", "arrow-up-right-from-square"]} />
-                  </a>
-                  <p>{note}</p>
-                </li>
-              );
-            })}
-          </ul>
-          <ul className={css.social}>
-            <li>
-              <h4>Social</h4>
-            </li>
-            <li className={css.socialList}>
+          <div className={css.social}>
+            <h4>Social</h4>
+            <div className={css.socialList}>
               {content.social.map(({ url, icon }, index) => {
                 return (
                   <a key={index} href={url} rel="noreferrer" target="_blank">
@@ -75,24 +41,9 @@ export default function Footer() {
                   </a>
                 );
               })}
-            </li>
-          </ul>
-        </section>
-        <section className={css.github}>
-          <ul className={css.social}>
-            <li>
-              <h4>Social</h4>
-            </li>
-            <li className={css.socialList}>
-              {content.social.map(({ url, icon }, index) => {
-                return (
-                  <a key={index} href={url} rel="noreferrer" target="_blank">
-                    <Icon icon={["fab", icon]} />
-                  </a>
-                );
-              })}
-            </li>
-          </ul>
+            </div>
+          </div>
+          <div>© Siddesh Bagare. All rights reserved.</div>
         </section>
       </Container>
       <canvas id="gradient-canvas" className={""} data-transition-in></canvas>
