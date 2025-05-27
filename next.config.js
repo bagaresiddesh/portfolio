d7ecf4023ff3a064755a327913bde182469a3279
Siddesh Bagare <sbagare@medforce.net>
2025-05-27 21:15:14 +0530

    deploy on github

--- next.config.js
+++ next.config.js
@@ -1,40 +1,9 @@
-module.exports = {
-	reactStrictMode: true,
-	env: {
-		dir: '/',
-	},
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: '**.vercel.app',
-				pathname: '/api/**'
-			},
-			{
-				protocol: 'https',
-				hostname: '**.vercel.app',
-				pathname: '?app=portfolio-theme-jqe0jhmif-atlamors.vercel.app'
-			},
-			{
-				protocol: 'https',
-				hostname: '**.shields.io',
-				pathname: '/badge/**'
-			},
-			{
-				protocol: 'https',
-				hostname: '**.shields.io',
-				pathname: '/github/**'
-			},
-			{
-				protocol: 'https',
-				hostname: '**.githubusercontent.com',
-				pathname: '/**'
-			},
-			{
-				protocol: 'https',
-				hostname: '**.medium.com',
-				pathname: '/**'
-			},
-		],
-	},
-}
+const isGithubPages = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  output: 'export',
+  basePath: isGithubPages ? '/portfolio' : '',
+  assetPrefix: isGithubPages ? '/portfolio/' : '',
+};
+
+module.exports = nextConfig;
