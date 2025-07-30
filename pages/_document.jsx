78cb5ac71fa953a5a489bba3e26a430c97d54b31
Siddesh Bagare <siddeshb@incubxperts.com>
2025-07-30 21:17:06 +0530

    last moment fixes

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -13,35 +13,43 @@ export default function Document () {
         <link
           rel='apple-touch-icon'
           sizes='180x180'
-          href='/favicon/apple-touch-icon.png'
+          href='/favicon/favicon.png'
         />
         <link
           rel='icon'
           type='image/png'
           sizes='32x32'
-          href='/favicon/favicon-32x32.png'
+          href='/favicon/favicon.png'
         />
         <link
           rel='icon'
           type='image/png'
           sizes='16x16'
-          href='/favicon/favicon-16x16.png'
+          href='/favicon/favicon.png'
         />
         <link rel='manifest' href='/favicon/site.webmanifest' />
         <link
           rel='mask-icon'
-          href='/favicon/safari-pinned-tab.svg'
+          href='/favicon/favicon.png'
           color='#5bbad5'
-        />
+        />{' '}
         <meta name='msapplication-TileColor' content='#da532c' />
-        <meta name='theme-color' content='#ffffff' />;
+        <meta name='theme-color' content='#ffffff' />
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function (c, l, a, r, i, t, y){
-                        c[a]=c[a]||function () {(c[a].q = c[a].q || []).push(arguments);
-                        t = l.createElement(r);t.async = 1;t.src = 'https://www.clarity.ms/tag/' + i;
-                        y = l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t, y);
-                        })(window, document, "clarity", "script", "sksv2795f1");`
+            __html: `(function (c, l, a, r, i, t, y) {
+  c[a] =
+    c[a] ||
+    function () {
+      ;(c[a].q = c[a].q || []).push(arguments)
+    }
+  t = l.createElement(r)
+  t.async = 1
+  t.src = 'https://www.clarity.ms/tag/' + i
+  y = l.getElementsByTagName(r)[0]
+  y.parentNode.insertBefore(t, y)
+})(window, document, 'clarity', 'script', 'sksv2795f1')
+`
           }}
         ></script>
       </Head>
